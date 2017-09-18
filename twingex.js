568146c22a6068ce5bc34741f6627d7ae6dc6f6d
Ben <ben.winding@gmail.com>
2017-09-18 23:29:53 +0930

    Initial version

---
+++ twingex.js
@@ -0,0 +1,12 @@
+window.storyFormat({
+  "name":"TwingeX",
+  "version":"0.1",
+  "author":'Ben (that guy) Winding',
+  "description":"The extremely simple 'twingex' format.",
+  "image":"icon.svg",
+  "url":"http://github.com/benwinding/twingex/",
+  "license":'MIT License',
+  "proofing":false,
+
+  "source":"<html>\r\n\t<head>\r\n\t\t<title>{{STORY_NAME}}</title>\r\n\t\t<meta charset=\"UTF-8\">\r\n\t\t</head>\r\n\t<body>\r\n\t\t<pre id=\"output\">\r\n\t\t\r\n\t\t</pre>\r\n\t\t<div id=\"storyData\" style=\"display: none;\">\r\n\t\t\t{{STORY_DATA}}\r\n\t\t</div>\r\n\t</body>\r\n</html>"
+});
