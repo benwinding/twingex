78710818edb1e6d845c817c82856b886702e45ca
Ben Winding <hello@benwinding.com>
2021-12-31 10:07:04 +1030

    Add build script

---
+++ build.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+// This compiles the output format file "/twingex.js", which is used by Twine2 
+
+const fs = require('fs');
+const path = require('path');
+const outputFilePath = path.join(__dirname, './twingex.js');
+
+function GetCurrentSemVer() {
+  const text = fs.readFileSync(outputFilePath).toString()
+  const versionStr = text.split('"version": "').pop();
+  if (!versionStr) {
+    throw new Error(`cannot find version string in "${outputFilePath}"`)
+  }
+  const semVer = versionStr.split('",').shift();
+  return semVer;
+}
+
+function GetNewVersion() {
+  const semVer = GetCurrentSemVer();
+  const [major, minor, patch] = semVer.split('.').map(v => +v);
+  const newSemVer = [major, minor, patch + 1].join('.')
+  return newSemVer;
+}
+
+function GetTemplateSource() {
+  const templatePath = path.join(__dirname, './templateSource.html');
+  const templateStr = fs.readFileSync(templatePath).toString();
+  return templateStr;
+}
+
+const outputObj = {
+  "name": "TwingeX",
+  "version": GetNewVersion(),
+  "author": "Ben Winding",
+  "description": "The extremely simple 'twingex' format.",
+  "image": "icon.svg",
+  "url": "http://github.com/benwinding/twingex/",
+  "license": "MIT License",
+  "proofing": false,
+  "source": GetTemplateSource(),
+};
+
+const outputStr = `window.storyFormat(${JSON.stringify(outputObj, null, 2)})`
+fs.writeFileSync(outputFilePath, outputStr)
